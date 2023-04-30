da69e46a09c2d1a1f1c61f2e5673a2ca211cb335
Saket Kedia <saketkedia8659@gmail.com>
2023-04-30 23:14:31 +0530

    commit

---
+++ src/main/java/com/mycompany/toturial_hub/Toturial_hub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.toturial_hub;
+
+/**
+ *
+ * @author SAKET KEDIA
+ */
+public class Toturial_hub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
